44ac47f5fcb60af2d0e39e857bd4be3c86c271a0
printfn <printfn@users.noreply.github.com>
2021-09-28 21:16:18 +1300

    Make sha1sum optional, improve error-checking

--- download-server-jar.sh
+++ download-server-jar.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Prerequisites:
+#   * curl
+#   * jq
+#   * sha1sum (optional, skips hash verification if not installed)
+
+# You can also run this script as:
+# bash <(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/printfn/minecraft-utils/main/download-server-jar.sh) latest
+
 if [[ "$#" == 0 ]]; then
   echo "Usage: $0 <version> (or one of 'latest', 'latest-snapshot', 'list')" >&2
   exit 1
 fi
 
-data=$(curl -s https://launchermeta.mojang.com/mc/game/version_manifest.json)
+data=$(curl -sS https://launchermeta.mojang.com/mc/game/version_manifest.json)
 
 if [[ "$1" == "latest" ]]; then
     version=$(echo "$data" | jq -r .latest.release)
@@ -22,12 +30,20 @@ fi
 echo "Downloading version $version..."
 
 url=$(echo "$data" | jq -r ".versions[] | select(.id == \"$version\") | .url")
-data=$(curl -s $url | jq .downloads.server)
+if [[ -z "$url" ]]; then
+    echo "Unknown version '$version'"
+    exit 1
+fi
+data=$(curl -Ss "$url" | jq .downloads.server)
 url=$(echo "$data" | jq -r .url)
 sha1=$(echo "$data" | jq -r .sha1)
-curl -o server.jar $url
-sha1sum --check <(echo "$sha1 server.jar") >/dev/null 2>/dev/null
-if [[ $? != 0 ]]; then
+curl -o server.jar "$url"
+
+if ! command -v sha1sum &>/dev/null; then
+    # sha1sum not installed, skip verification
+    exit
+fi
+if ! sha1sum --check <(echo "$sha1 server.jar") >/dev/null 2>/dev/null; then
     echo "Error: checksum mismatch" >&2
     exit 1
 fi
