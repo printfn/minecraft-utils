91843efc751045d6504c52b50e564cbfb7eed426
printfn <printfn@users.noreply.github.com>
2022-02-04 14:06:21 +1300

    Clean up shell scripts some more

--- download-server-jar.sh
+++ download-server-jar.sh
@@ -9,31 +9,58 @@ set -euo pipefail
 # You can also run this script as:
 # bash <(curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/printfn/minecraft-utils/main/download-server-jar.sh) latest
 
-if [[ "$#" == 0 ]]; then
-  echo "Usage: $0 <version> (or one of 'latest', 'latest-snapshot', 'list', 'list-latest', 'list-latest-snapshot')" >&2
-  exit 1
+usage="Usage: $0 [-q|--quiet] <version> (or one of 'latest', 'latest-snapshot', 'list', 'list-latest', 'list-latest-snapshot')"
+
+quiet=false
+foundcmd=false
+
+while [[ "$#" != 0 ]]; do
+    arg="$1"
+    if [[ "$arg" == "-q" || "$arg" == "--quiet" ]]; then
+        quiet=true
+    elif [[ "$arg" == "-h" || "$arg" == "--help" ]]; then
+        echo "$usage"
+        exit
+    elif [[ "$arg" =~ ^- ]]; then
+        echo "Unknown option '$arg'" >&2
+        exit 1
+    elif [[ "$foundcmd" == false ]]; then
+        command="$arg"
+        foundcmd=true
+    else
+        echo "Too many arguments" >&2
+        exit 1
+    fi
+    shift
+done
+
+if [[ "$foundcmd" == false ]]; then
+    echo "$usage" >&2
+    exit 1
 fi
 
 data=$(curl -sS https://launchermeta.mojang.com/mc/game/version_manifest.json)
 
-if [[ "$1" == "latest" ]]; then
+if [[ "$command" == "latest" ]]; then
     version=$(echo "$data" | jq -r .latest.release)
-elif [[ "$1" == "list-latest" ]]; then
+elif [[ "$command" == "list-latest" ]]; then
     echo "$data" | jq -r ".latest.release"
     exit
-elif [[ "$1" == "latest-snapshot" ]]; then
+elif [[ "$command" == "latest-snapshot" ]]; then
     version=$(echo "$data" | jq -r .latest.snapshot)
-elif [[ "$1" == "list-latest-snapshot" ]]; then
+elif [[ "$command" == "list-latest-snapshot" ]]; then
     echo "$data" | jq -r ".latest.snapshot"
     exit
-elif [[ "$1" == "list" ]]; then
+elif [[ "$command" == "list" ]]; then
     echo "$data" | jq -r ".versions[].id"
     exit
 else
-    version="$1"
+    version="$command"
 fi
 
-echo "Downloading version $version..."
+if [[ "$quiet" != true ]]; then
+    echo "Downloading version $version..."
+fi
 
 url=$(echo "$data" | jq -r ".versions[] | select(.id == \"$version\") | .url")
 if [[ -z "$url" ]]; then
@@ -43,7 +70,13 @@ fi
 data=$(curl -Ss "$url" | jq .downloads.server)
 url=$(echo "$data" | jq -r .url)
 sha1=$(echo "$data" | jq -r .sha1)
-curl -o server.jar "$url"
+
+curlsilent=""
+if [[ "$quiet" == true ]]; then
+    curlsilent="--silent"
+fi
+
+curl $curlsilent -o server.jar "$url"
 
 if ! command -v sha1sum &>/dev/null; then
     # sha1sum not installed, skip verification
--- start-server.sh
+++ start-server.sh
@@ -15,18 +15,18 @@ confirm() {
 }
 
 echo "Checking if a 'Minecraft' security group exists..."
-SG_INFO="$(aws --region $REGION ec2 describe-security-groups \
+SG_INFO="$(aws --region "$REGION" ec2 describe-security-groups \
     --filters "Name=group-name,Values=Minecraft" | jq ".SecurityGroups")"
 
-if [[ "$(echo $SG_INFO | jq length)" == "1" ]]; then
-    SG_ID=$(echo $SG_INFO | jq -r ".[0].GroupId")
-    VPC_ID=$(echo $SG_INFO | jq -r ".[0].VpcId")
+if [[ "$(echo "$SG_INFO" | jq length)" == "1" ]]; then
+    SG_ID=$(echo "$SG_INFO" | jq -r ".[0].GroupId")
+    VPC_ID=$(echo "$SG_INFO" | jq -r ".[0].VpcId")
     echo "Found security group $SG_ID in VPC $VPC_ID"
 else
     echo "Not found"
     echo "Finding default VPC..."
-    VPC_INFO="$(aws --region $REGION ec2 describe-vpcs --filters "Name=is-default,Values=true")"
-    if [[ "$(echo $VPC_INFO | jq ".Vpcs | length")" == "1" ]]; then
+    VPC_INFO="$(aws --region "$REGION" ec2 describe-vpcs --filters "Name=is-default,Values=true")"
+    if [[ "$(echo "$VPC_INFO" | jq ".Vpcs | length")" == "1" ]]; then
         VPC_ID=$(echo "$VPC_INFO" | jq -r ".Vpcs[0].VpcId")
         echo "Found default VPC: $VPC_ID"
     else
@@ -35,20 +35,20 @@ else
     fi
 
     echo "Creating 'Minecraft' security group..."
-    SG_ID="$(aws --region $REGION ec2 create-security-group \
+    SG_ID="$(aws --region "$REGION" ec2 create-security-group \
         --group-name Minecraft \
         --description "Minecraft Server (and SSH access)" \
         --vpc-id "$VPC_ID" | jq -r .GroupId)"
 
     echo "Enabling SSH access..."
-    aws --region $REGION ec2 authorize-security-group-ingress \
+    aws --region "$REGION" ec2 authorize-security-group-ingress \
         --group-id "$SG_ID" \
         --protocol tcp \
         --port 22 \
         --cidr 0.0.0.0/0 >/dev/null
 
     echo "Enabling Minecraft access..."
-    aws --region $REGION ec2 authorize-security-group-ingress \
+    aws --region "$REGION" ec2 authorize-security-group-ingress \
         --group-id "$SG_ID" \
         --protocol tcp \
         --port 25565 \
@@ -61,20 +61,20 @@ fi
 # aws ssm get-parameters-by-path --path /aws/service/ami-amazon-linux-latest --query "Parameters[].Name"
 
 echo "Launching server..."
-EC2_INFO=$(aws --region $REGION ec2 run-instances \
+EC2_INFO=$(aws --region "$REGION" ec2 run-instances \
     --image-id resolve:ssm:/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-arm64-gp2 \
     --instance-type t4g.medium \
-    --key-name $REGION \
+    --key-name "$REGION" \
     --security-group-ids "$SG_ID" \
     --associate-public-ip-address)
 
-EC2_ID=$(echo $EC2_INFO | jq -r ".Instances[0].InstanceId")
+EC2_ID=$(echo "$EC2_INFO" | jq -r ".Instances[0].InstanceId")
 
 echo "Waiting for server to come online..."
-aws --region $REGION ec2 wait instance-running --instance-ids $EC2_ID
+aws --region "$REGION" ec2 wait instance-running --instance-ids "$EC2_ID"
 
 echo "Retrieving IP address..."
-SERVER_IP=$(aws --region $REGION ec2 describe-instances --instance-ids $EC2_ID \
+SERVER_IP=$(aws --region "$REGION" ec2 describe-instances --instance-ids "$EC2_ID" \
     | jq -r ".Reservations[0].Instances[0].PublicIpAddress")
 
 echo "Launched server $EC2_ID (IP: $SERVER_IP)"
@@ -107,11 +107,11 @@ curl -s --fail-with-body -X PATCH "https://api.cloudflare.com/client/v4/zones/$Z
     --data "{\"content\":\"$SERVER_IP\",\"ttl\":60,\"proxied\":false}" >/dev/null
 
 echo "Connecting to server..."
-ssh -i ~/.ssh/$REGION.pem \
+ssh -i "$HOME/.ssh/$REGION.pem" \
     -o "UserKnownHostsFile=/dev/null" \
     -o "StrictHostKeyChecking=accept-new" \
     -o "LogLevel=ERROR" \
-    ec2-user@$SERVER_IP "
+    "ec2-user@$SERVER_IP" "
 
     echo \"Successfully connected to server\"
     aws configure set aws_access_key_id $(aws configure get aws_access_key_id --profile default)
@@ -153,8 +153,8 @@ ssh -i ~/.ssh/$REGION.pem \
 confirm "Shut down server?"
 
 echo "Terminating server..."
-aws --region $REGION ec2 terminate-instances --instance-ids $EC2_ID >/dev/null
-aws --region $REGION ec2 wait instance-terminated --instance-ids $EC2_ID
+aws --region "$REGION" ec2 terminate-instances --instance-ids "$EC2_ID" >/dev/null
+aws --region "$REGION" ec2 wait instance-terminated --instance-ids "$EC2_ID"
 
 echo "Deleting security group..."
-aws --region $REGION ec2 delete-security-group --group-id $SG_ID
+aws --region "$REGION" ec2 delete-security-group --group-id "$SG_ID"
